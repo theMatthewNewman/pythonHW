d2a152fb3cca1e87d601bab50ec0b3753b54c3ae
Matthew Newman <theMatthewNewman@gmail.com>
2022-10-18 13:03:19 -0400

    started oop

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from pyamaze import maze, agent
 import aStar
 
-
+class mazeSolver:
+    def __init__(self, maz, gui):
+        m = maze()
+        m.CreateMaze(loadMaze=maz)
+        self.gui = gui
+        m.run()
 m = maze()
 m.CreateMaze(loadMaze="./maze--2022-10-11--16-50-26.csv")
+print(m)
 
 path =aStar.aStar(m)
 print(path)
@@ -14,3 +20,5 @@ a=agent(m,footprints=True)
 m.tracePath({a:path})
 
 m.run()
+if __name__ == "__main__":
+    mazeSolver("./maze--2022-10-11--16-50-26.csv",False)
