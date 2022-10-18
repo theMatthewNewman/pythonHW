18f4a59ffb8f4c6d2e5ecbf94449c50d6ebbb152
Matthew Newman <theMatthewNewman@gmail.com>
2022-10-18 17:17:48 -0400

    edited parser

--- txtParser.py
+++ txtParser.py
@@ -1,27 +1,14 @@
-import shlex
-import re
-import csv
-from pyamaze import maze,COLOR,agent
+from pyamaze import maze
  
 
 class convert:
 
     def __init__(self, infile, outfile):
-        # Variable Initialization and Functions too
-        self.outfile=outfile
         self.file = open(infile, 'r')
         lines = self.parse_func(self.file.readlines())
         dims = self.get_dimensions(lines)
-        self.rows = dims[0]
-        self.cols = dims[1]
-        # read the input file and get tokenizer for dictionary list
-        self.arr = [[[0 for x in range(4)] for y in range(self.cols)] for z in range(self.rows)]
-        self.compute_arr(lines)
-        self.write_csv()
-        # process the dictionary list and sort the dictionary list
-        # before write the list into a cvs file (self.csvfile)
-    # this function will take an input line and parse into 5 fields
-    # field 1: cell., field 2: north distance...
+        arr = self.compute_arr(lines, dims)
+        self.write_csv(arr, dims, outfile)
 
     def parse_func(self,lines):
         parsedLines = []
@@ -30,8 +17,6 @@ class convert:
             temp = temp.split()
             parsedLines.append(temp)
         return parsedLines
-
-    # Collecting number of columns and rows
 
     def get_dimensions(self, lines):
         rows=0
@@ -43,8 +28,8 @@ class convert:
                 cols = int(line[1])
         return (rows,cols)
 
-    # Computing and organizing array
-    def compute_arr(self,lines):
+    def compute_arr(self,lines, dim):
+        arr = [[[0 for x in range(4)] for y in range(dim[0])] for z in range(dim[1])]
         for line in lines:
             row = int(line[0])-1
             col = int(line[1])-1
@@ -54,25 +39,25 @@ class convert:
                 else:
                     line[i] = 0
 
-            self.arr[row][col] = line[2:]
+            arr[row][col] = line[2:]
+        return(arr)
 
-#Writing to new csv with proper formatting for loading into pyamaze maze generation
-    def write_csv(self):
-        output = open(self.outfile, "w")
+    def write_csv(self, dic, dim, outfile):
+        output = open(outfile, "w")
         output.write("cell,E,W,N,S")
-        for j in range(self.cols):
-            for i in range(self.rows):
+        for j in range(dim[1]):
+            for i in range(dim[0]):
                 # Writing data in proper formatting
-                output.write('\n"({}, {})",{},{},{},{}'.format((i+1),(j+1),self.arr[i][j][0],self.arr[i][j][1],self.arr[i][j][2],self.arr[i][j][3]))        
+                output.write('\n"({}, {})",{},{},{},{}'.format((i+1),(j+1),dic[i][j][0],dic[i][j][1],dic[i][j][2],dic[i][j][3]))        
 
-    def createMaze(self):
+    def createMaze(self, outfile):
         # maze created based the self.csvfile
         m = maze()
-        m.CreateMaze( loadMaze=self.outfile)
+        m.CreateMaze(loadMaze=outfile)
         # displace the maze based the created maze
         m.run()
  
 
 if __name__=="__main__":
     a = convert('inputfile3.txt','tmp.csv')
-    a.createMaze()
+    a.createMaze('tmp.csv')
